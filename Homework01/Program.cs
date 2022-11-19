77697c87116d266ac35c9e76008a354e0b15c72f
ASGSPb <118271073+ASGSPb@users.noreply.github.com>
2022-11-19 14:36:16 +0300

    HW1

---
+++ Homework01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+Console.WriteLine("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+// на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
