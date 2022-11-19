f096249efd0810140af68a153115893ad4f381eb
ASGSPb <118271073+ASGSPb@users.noreply.github.com>
2022-11-19 17:21:01 +0300

    HW1

--- Homework01/Program.cs
+++ Homework01/Program.cs
@@ -1,4 +1,11 @@
-﻿Console.Clear();
+﻿// на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
 Console.WriteLine("Введите число");
 Console.WriteLine("a = ");
 int a = Convert.ToInt32(Console.ReadLine());
@@ -9,21 +16,15 @@ if ( a != b)
 {
 if ( a < b)
 {
-   Console.WriteLine("a < b");
+   Console.WriteLine("a < b    " + a + " < " + b);
 }
 else
 {
-  Console.WriteLine("a > b");
+  Console.WriteLine("a > b  " + a + " > " + b);
   }
 }
 else 
 {
-     Console.WriteLine("a = b"); 
+     Console.WriteLine("a = b       " + a + " = " + b); 
 }
 
-// на вход принимает два числа и выдаёт,
-// какое число большее, а какое меньшее.
-
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
