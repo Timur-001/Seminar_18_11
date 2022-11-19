f2867b50cf771dfc66613dbc9ff29af99ecd49bb
ASGSPb <118271073+ASGSPb@users.noreply.github.com>
2022-11-19 18:20:18 +0300

    HW4

---
+++ Homework04/Program.cs
@@ -0,0 +1,15 @@
+﻿// на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.WriteLine("Введите число N =");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+if ( ( N + count ) == 9)
+{
+    Console.WriteLine("Число четное?  НЕТ!");
+}
+else Console.WriteLine("Число четное?  ДА!");
